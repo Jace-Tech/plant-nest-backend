3cff8e71ae48393534e9de892351f2e8c96d5c1c
Ugly Ceaser <74794170+ugly-ceaser@users.noreply.github.com>
2023-08-11 11:30:08 +0100

    stashing to pull update

--- app/migrate.py
+++ app/migrate.py
@@ -2,6 +2,9 @@ from .database.user_table import create_user_table
 from .database.admin_table import create_admin_table
 from .database.plant_table import create_plant_table
 from .database.category_table import create_category_table
+from .database.feedback_table import create_feedback_table
+from .database.cart_table import create_product_cart_table
+from .database.wishlist_table import create_product_wishlist_table
 
 
 # RUN MIGRATIONS
@@ -10,3 +13,6 @@ def run_migrations():
     create_user_table()
     create_admin_table()
     create_plant_table()
+    create_feedback_table()
+    create_product_cart_table()
+    create_product_wishlist_table()
